23a142cb00a72310f77e35aad5fb30f209157031
SuQinghang <qinghang_su@163.com>
2020-12-11 13:41:06 +0800

    实现results.html中搜索框点击并查询

--- app.py
+++ app.py
@@ -9,7 +9,16 @@ app.config['SECRET_KEY']="123"
 # 接收关键词，返回从服务器查询得到的JSON数据
 def search_from_server(keywords):
     # 从服务器查询
-    results = [{keywords: 'result of search ' + keywords}]
+    # 接上es
+    # 把kw的查询传到es
+    # 得到es的结果:总共结果数， positive/negative， positive/negative:{datetime(yyyy-mm): num},
+    # json(前20个结果)[text, pos/neg, create_at, screen_name]
+    file = 'aaa.json'
+    with open(file, encoding='utf-8') as f:
+        data = json.load(f)
+    results = data['hits']['hits']
+    # print(data)
+    # results = [{keywords: 'result of search ' + keywords}]
     return results
 
 
@@ -22,15 +31,40 @@ def index():
 def getSearch():
     form = SearchForm()
     search = request.args.get('query', '')
-
-    if search=="":
+    # print(search)
+    if search == "":
         # 显示提示框
         return render_template("index.html")
     else:
         results = search_from_server(search)
-        print(results)
-        return render_template("result.html", results=results)
+        # 计算以datetime聚合的positive/negative
+        # correspond = search_correspond
+        total = 1000
+        positive = 500
+        negative = 500
+
+        return render_template("results.html", form=form, search=search, results=results)
+
+
+@app.route('/getSearch_results', methods=['GET', 'POST'])
+def getSearch_results():
+    form = SearchForm()
+    search = request.args.get('query', '')
+    print('input: ', search)
+    if search == "":
+        # 显示提示框
+        return render_template("results.html")
+    else:
+        results = search_from_server(search)
+        # 计算以datetime聚合的positive/negative
+        # correspond = search_correspond
+
+        return render_template("results.html", form=form, search=search, results=results)
 
 
 if __name__ == '__main__':
-    app.run(port=8080)
+    # app.run()
+    #----------------------------------------------------
+    # search_from_server测试
+    results = search_from_server('')
+    #----------------------------------------------------
